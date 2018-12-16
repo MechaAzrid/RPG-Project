e62c39b39986eed6629d379383844228a7f4707d
Mark <1007004@student.sae.edu.au>
2018-12-16 19:44:05 +1100

    Working on the Dialogue System

---
+++ RPG Project/Assets/Scripts/DialogueHolder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueHolder : MonoBehaviour
+{
+
+    public string dialogue;
+    private DialogueManager dialogueManager;
+
+    void Start()
+    {
+        dialogueManager = FindObjectOfType<DialogueManager>();
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Testing Character")
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                dialogueManager.DialogueDisplayed(dialogue);
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ RPG Project/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    public GameObject dialogueBox;
+    public Text dialogueText;
+
+    public bool dialogueActive;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //if (dialogueActive && Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    dialogueBox.SetActive(false);
+        //    dialogueActive = false;
+        //}
+    }
+
+    public void DialogueDisplayed(string dialogue)
+    {
+        dialogueActive = true;
+        dialogueBox.SetActive(true);
+        dialogueText.text = dialogue;
+    }
+}
